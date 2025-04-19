421114834e80ff096a93a546ab581d714785061d
Ezequiel-Amorim <contaezequielamorim@gmail.com>
2025-04-19 10:51:01 -0300

    Update tabela2.py

--- tabela2.py
+++ tabela2.py
@@ -1,18 +1,23 @@
 from tabela import escolha
 from rota import linhas
 from time import sleep
+from tabulate import tabulate
+
 linhas()
 print('Coloque suas informações')
 sleep(1)
+
 nome = input('Nome : ')
 idade = int(input('Idade :'))
 profissao = input('Profissão :')
+
 print('''Quer colocar seu salário ? : R$
 	  [ 1 ] SIM
 	  [ 2 ] NÃO''')
-opçao = int(input('Escolha :'))
-if opçao == 1:
-	salario = float(input('R$ ')) 
+opcao = int(input('Escolha :'))
+
+if opcao == 1:
+	salario = f"R$ {float(input('R$ ')):.2f}"
 else:
 	print('Opção não registrada')
 	salario = 'Não informado'
@@ -20,5 +25,13 @@ else:
 sleep(1)
 print('Registro adicionado')
 print('--------------------')
-print(f'Cliente: {nome}\nIdade: {idade} anos\nProfissão: {profissao}\nSalário: R$ {salario}')
+
+dados = [
+	['Cliente ', nome],
+	['Idade ', f'{idade} anos'],
+	['Profissão ', profissao],
+	['Salário ', salario]
+]
+
+print(tabulate(dados, tablefmt='grid'))
 linhas()
